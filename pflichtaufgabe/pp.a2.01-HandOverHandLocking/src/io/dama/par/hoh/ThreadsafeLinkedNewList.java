73a376a7206c37c95dc236039ddb048aa5a03454
1627143 <1627143@stud.hs-mannheim.de>
2017-12-28 09:08:47 +0100

    versuch des Hand-over-Hand Lockings

---
+++ pflichtaufgabe/pp.a2.01-HandOverHandLocking/src/io/dama/par/hoh/ThreadsafeLinkedNewList.java
@@ -0,0 +1,92 @@
+package io.dama.par.hoh;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ThreadsafeLinkedNewList<T> implements NewList<T> {
+    private class ListElement<T> {
+        private T                    element;
+        private ListElement<T>       prev;
+        private final ListElement<T> next;
+        public ReentrantLock lock = new ReentrantLock();;
+
+        private ListElement(final T element, final ListElement<T> prev, final ListElement<T> next) {
+            this.element = element;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
+    private ListElement<T> first;
+
+    public ThreadsafeLinkedNewList() {
+        this.first = null;
+    }
+
+    @Override
+    public T get(final int i) {
+    	int j = 0;
+        ListElement<T> ptr =this.first;
+        
+        Lock current = ptr.lock;
+        while (j++ < i) {
+        	// das aktuelle Element wird gelockt
+        	current.lock();
+        	// das nächste Listenelement wird kurzfristig gelockt
+        	Lock next = ptr.next.lock;
+        	next.lock();
+        	// das aktuelle Listenelement wird das aktuelle Listenelement
+        	ptr = ptr.next;
+        	// das aktuelle Element wird freigegeben
+        	current.unlock();
+        	// das vorherige Element wird wieder freigegeben
+        	current = next;
+        	
+        }        
+        try{
+        	return ptr.element;
+        }
+        finally{
+        	// Freigabe des ausgegebenen Elementes
+        	current.unlock();
+        }
+        
+    }
+
+    // einfügen an erster Stelle
+    @Override
+    public void add(final T e) {
+        final ListElement<T> insert = new ListElement<>(e, null, this.first);
+        if (this.first != null) {
+        	// erste Element locken
+        	Lock current = this.first.lock;
+        	current.lock();
+            // vor dem ersten Element Einfügen
+        	this.first.prev = insert;
+            // Lock vom ersten Element entfernen
+        	current.unlock();
+        }
+        this.first = insert;       
+    }
+
+    // Ändern des Wertes an bstimmter Position
+    @Override
+    public void mod(final int i, final T e) {
+        int j = 0;
+        ListElement<T> ptr = this.first;
+        
+        //gleiche Prinzip wie beim get
+        Lock current = ptr.lock;       
+        while (j++ < i) {
+        	current.lock();
+        	Lock next = ptr.next.lock;
+        	next.lock();
+            ptr = ptr.next;
+            current.unlock();
+        	current = next;
+        }
+        ptr.element = e;
+        current.unlock();
+    }
+
+}
